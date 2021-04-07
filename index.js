a4c719816e139acd9cd3fa60e2959d2a485d5d0b
Jay <jaygandhi129@gmail.com>
2021-04-07 16:15:32 +0530

    Added passport to Login

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const bodyParser = require("body-parser")
@@ -10,15 +10,18 @@ const bcrypt = require('bcrypt');
 const ejs = require("ejs")
 const mysql = require('mysql')
 const multer = require("multer");
-const { connect } = require("http2");
 const func = require('./func');
 const flash = require('express-flash')
 const session = require('express-session')
 const passport = require('passport')
+const method_override = require('method-override');
 
 
 app.use(bodyParser.json());
- app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(method_override('_method'));
 //Used to access static files from public folder
 app.use(express.static("public"));
 
@@ -36,20 +39,20 @@ app.use(passport.session())
 
 //Establishing Connection to database
 var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_SCHEMA,
-    dateStrings: 'date'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_SCHEMA,
+  dateStrings: 'date'
 });
 
 connection.connect(function (error) {
-    if (error) {
-        console.log("Error in Connecting Database");
-        throw error;
-    } else {
-        console.log("Connected to Database");
-    }
+  if (error) {
+    console.log("Error in Connecting Database");
+    throw error;
+  } else {
+    console.log("Connected to Database");
+  }
 });
 
 
@@ -57,119 +60,123 @@ connection.connect(function (error) {
 
 // passport configure
 
-
-
 const initializePassport = require('./passport-config');
-initializePassport(passport,
-   mobile=>{
-  const query = "SELECT sPhoneNo from seller_details where sPhoneNo = ?";
-  var phone;
-  connection.query(query,parseInt(mobile),function(err, rows){
-    if (err){
-      console.log(err);
-    }else{
-      phone = rows[0].sPhoneNo;
-    }
-  })
-  
-  console.log(phone);
-  return phone;
-},id=>{
-  var id;
-  const query = "SELECT sId from seller_details where sPhoneNo = ?";
-  connection.query(query,mobile,function(err, rows){
-    if (err){
-      console.log(err);
-    }else{
-      id = rows[0].sId;
-    }
-  })
-  return id;
-})
+initializePassport(passport);
+
+
+function checkAuthenticated(req, res, next) {
+  if(req.isAuthenticated()){
+    return next()
+  }
+  else{
+    res.redirect('/business/login');
+  }
+}
+function checkNotAuthenticated(req, res, next) {
+  if(req.isAuthenticated()){
+    res.redirect('/dashboard');
+  }
+  else{
+    next()
+  }
+}
 
 
 
-
-
-
-app.get("/",function(req,res){
- res.send("Hi");
+app.get("/", function (req, res) {
+  res.send("Hi");
 });
 
 /************************************Seller Starts*************************************************/
 
 //Seller Home Page
 app.get("/business", function (req, res) {
-    res.render('index');
+  res.render('index');
 });
 
 //Seller Register Page-1
-app.get("/business/register",function(req, res){
-    res.render('sellerRegister1',{flag :false});
+app.get("/business/register",checkNotAuthenticated, function (req, res) {
+  res.render('sellerRegister1', {
+    flag: false
+  });
 })
 
-app.post("/business/register/home",function(req,res){
-  var b_name=req.body.b_name;
-  var b_owner=req.body.b_owner;
-  var b_mobile=parseInt(req.body.b_mobile);
- res.render("sellerRegister1",{flag :true ,b_name,b_owner,b_mobile});
+app.post("/business/register/home", checkNotAuthenticated,function (req, res) {
+  var b_name = req.body.b_name;
+  var b_owner = req.body.b_owner;
+  var b_mobile = parseInt(req.body.b_mobile);
+  res.render("sellerRegister1", {
+    flag: true,
+    b_name,
+    b_owner,
+    b_mobile
+  });
 })
 
-app.post("/business/register/nextstep",async (req,res) => {
-    var data = req.body;
-    var sPassword = await bcrypt.hash(data.sPassword,10);
-    var query = "INSERT INTO seller_details (sName, sPhoneNo, sDOB, sGender, sAddress, sCity, sState, sZip, sAadhar, sPAN, sPassword) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    var list = [data.sName, data.sPhoneNo, data.sDOB, data.sGender, data.sAddress, data.sCity, data.sState, data.sZip, data.sAadhar, data.sPAN, data.sPassword]
-    connection.query(query, list, function(err, rows){
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log("Successfully inserted seller data.");
-            var seller;
-            connection.query("SELECT sId from seller_details where sPhoneNo = ?",data.sPhoneNo, function(err, row){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    seller = row[0].sId;
-                    var query2 = "INSERT INTO business_details (seller, bName, bCategory, bMobile, bGST,bEmail, bWebsite, bAddress, bCity, bState, bZip) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                    var list = [seller, data.bName, data.bCategory, data.bMobile, data.bGST,data.bEmail, data.bWebsite, data.bAddress, data.bCity, data.bState, data.bZip];
-                    connection.query(query2, list,function(err){
-                        if(err){
-                            console.log(err);
-                        }
-                        else{
-                            console.log("Successfully inserted business data.");
-                            func.sendmail(data.bEmail);
-                            res.redirect("/business/register/success");
-                        }
-                    });
-
-                }
-            });
+app.post("/business/register/nextstep", checkNotAuthenticated,async (req, res) => {
+  var data = req.body;
+  var sPassword = await bcrypt.hash(data.sPassword, 10);
+  var query = "INSERT INTO seller_details (sName, sPhoneNo, sDOB, sGender, sAddress, sCity, sState, sZip, sAadhar, sPAN, sPassword) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  var list = [data.sName, data.sPhoneNo, data.sDOB, data.sGender, data.sAddress, data.sCity, data.sState, data.sZip, data.sAadhar, data.sPAN, data.sPassword]
+  connection.query(query, list, function (err, rows) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully inserted seller data.");
+      var seller;
+      connection.query("SELECT sId from seller_details where sPhoneNo = ?", data.sPhoneNo, function (err, row) {
+        if (err) {
+          console.log(err);
+        } else {
+          seller = row[0].sId;
+          var query2 = "INSERT INTO business_details (seller, bName, bCategory, bMobile, bGST,bEmail, bWebsite, bAddress, bCity, bState, bZip) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+          var list = [seller, data.bName, data.bCategory, data.bMobile, data.bGST, data.bEmail, data.bWebsite, data.bAddress, data.bCity, data.bState, data.bZip];
+          connection.query(query2, list, function (err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Successfully inserted business data.");
+              func.sendmail(data.bEmail);
+              res.redirect("/business/register/success");
+            }
+          });
 
         }
-    });
-  });
-  //Seller Register Success Page
-  app.get("/business/register/success",function(req, res){
-    res.render('success_bregister');
-  });
+      });
 
-  // Seller Login Starts //
-  app.get("/business/login",function (req, res) {
-      res.render('sellerLogin');
+    }
   });
+});
+//Seller Register Success Page
+app.get("/business/register/success", checkNotAuthenticated,function (req, res) {
+  res.render('success_bregister');
+});
 
-  app.post("/business/login", passport.authenticate('local',{
-    successRedirect: '/dashboard',
-    failureRedirect: '/business/login',
-    failureFlash: true
-  }))
+// Seller Login Starts //
+app.get("/business/login", checkNotAuthenticated,function (req, res) {
+  res.render('sellerLogin');
+});
+
+
+app.post("/business/login", checkNotAuthenticated,passport.authenticate('local', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/business/login',
+  failureFlash: true
+}));
+
+app.delete('/logout',(req, res) => {
+  req.logOut();
+  res.redirect('/business/login');
+})
+
+
+//Seller Dashboard Starts////////////////
+app.get('/dashboard',checkAuthenticated,function(req, res){
+  res.render('dashboard',{name:req.user.sName});
+});
 
 /************************************Seller Ends*************************************************/
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Connected at 3000");
+  console.log("Connected at 3000");
 })
--- passport-config.js
+++ passport-config.js
@@ -1,32 +1,45 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
+const mysql = require('mysql')
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_SCHEMA,
+  dateStrings: 'date'
+});
+
+connection.connect();
 
 
-function initialize(passport, getUserByMobile, getUserById){
-  const authenticateUser = async (mobile,password,done)=>{
-    const user =  getUserByMobile(mobile)
-    console.log(mobile);
-  if (user == null){
-    return done(null, false, {message: 'No user with that Mobile Number'})
-  }
-  try{
-    if( await bcrypt.compare(password, user.password)){
+function initialize(passport){
 
-    }else{
-      return done(null, false, {message : 'Password Incorrect'})
-    }
-  } catch(e){
-    return done(e)
-  }
+  passport.use(new LocalStrategy({usernameField: 'mobile',passwordField: 'password',passReqToCallback : true},
+  function(req,mobile,password,done){
+    connection.query('SELECT * from seller_details where sPhoneNo = ?',mobile,function(err,rows){
+      if(err){
+        return done(err);
+      }
+      if (!rows.length) {
+        console.log("User not found");
+        return done(null, false, {'message':'No user found'}); 
+    } 
+    if (password !== rows[0].sPassword){
+      console.log("Password wrong "+ password + rows[0].sPassword);
+        return done(null, false, {'message':'Oops! Wrong password.'});}
+      else{
+        return done(null, rows[0]);	
+      }
+    })
 }
-
-  passport.use(new LocalStrategy({usernameField: 'mobile'},authenticateUser))
-  passport.serializeUser((user,done)=>done(null,user.id))
-  passport.deserializeUser((id,done)=>{
-    return done(null,getUserById(id))
-
-  })
-
+  ))
+  passport.serializeUser((user,done)=>done(null,user.sId))
+  passport.deserializeUser(function(sId, done) {
+		connection.query("select * from seller_details where sId = "+sId,function(err,rows){	
+			done(err, rows[0]);
+		});
+    });
 }
 
 module.exports = initialize
