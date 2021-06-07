1ab021373f184bd15fd663b0f572d8e326dc7d0c
Jay <jaygandhi129@gmail.com>
2021-06-07 15:04:44 +0530

    Fixed known bugs

--- index.js
+++ index.js
@@ -128,9 +128,24 @@ function custCheckNotAuthenticated(req, res, next) {
 
 
 /////////////////////Firebase and Multer Configure///////////////////////////////
-var serviceAccount = require(process.env.FIREBASE_SERVICEACC_KEY);
+// var serviceAccount = require(process.env.FIREBASE_SERVICEACC_KEY);
+var serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": process.env.CLIENT_URL,
+}
+// admin.initializeApp({
+//     credential: admin.credential.cert(require(process.env.FIREBASE_SERVICEACC_KEY))
+// });
 admin.initializeApp({
-    credential: admin.credential.cert(require(process.env.FIREBASE_SERVICEACC_KEY))
+    credential: admin.credential.cert(serviceAccount)
 });
 
 //admin.initializeApp(firebaseConfig);
