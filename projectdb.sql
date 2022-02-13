6793da4347f9071f7be53b30b375dba854a27cdf
Saurabh Dhoble <dhoblesaurabh35@gmail.com>
2022-02-13 16:17:14 +0530

    db uploaded

--- projectdb.sql
+++ projectdb.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
--- Host: localhost    Database: projectdb
+-- Host: localhost    Database: project_db
 -- ------------------------------------------------------
 -- Server version	8.0.27
 
@@ -14,6 +14,31 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_details`
+--
+
+DROP TABLE IF EXISTS `admin_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_details` (
+  `adminId` varchar(10) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` int NOT NULL DEFAULT '2',
+  PRIMARY KEY (`adminId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_details`
+--
+
+LOCK TABLES `admin_details` WRITE;
+/*!40000 ALTER TABLE `admin_details` DISABLE KEYS */;
+INSERT INTO `admin_details` VALUES ('admin1','10b8e822d03fb4fd946188e852a4c3e2',2);
+/*!40000 ALTER TABLE `admin_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `business_details`
@@ -508,4 +533,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-02-12 13:09:32
+-- Dump completed on 2022-02-13 16:16:46
